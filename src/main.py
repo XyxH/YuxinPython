b88eacb4202fa5950610b2f85144ed81e870053f
nainiucat <1415609050@qq.com>
2021-02-24 10:23:20 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 app=Flask(__name__)
+@app.route("/index",methods=["get","post"])
+def index():
+    return "HELLO WORLD!"
 
 if __name__ == '__main__':
     app.run(debug=True,host="localhost",port=10000)
